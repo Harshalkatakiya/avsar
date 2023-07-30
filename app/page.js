02fce0992df169fd5deb23a78e9fda10dd08c373
Prakash Gujarati <prakashgujarati.work@gmail.com>
2023-07-30 21:35:29 +0530

    registration closed

--- app/page.js
+++ app/page.js
@@ -4,139 +4,140 @@ import InputRow from "./InputRow";
 import axios from 'axios';
 
 const page = () => {
-  const [formData, setFormData] = useState({
-    full_name1: "",
-    email1: "",
-    phone_number1: "",
-    level1: "",
-    department1: "",
-    semester1: "",
-    enrollment1: "",
-    institute1: "",
-    eventtype: "",
-    eventName: "",
-  });
-  const [typeofevent, setTypeOfEvent] = useState("solo");
+  // const [formData, setFormData] = useState({
+  //   full_name1: "",
+  //   email1: "",
+  //   phone_number1: "",
+  //   level1: "",
+  //   department1: "",
+  //   semester1: "",
+  //   enrollment1: "",
+  //   institute1: "",
+  //   eventtype: "",
+  //   eventName: "",
+  // });
+  // const [typeofevent, setTypeOfEvent] = useState("solo");
 
-  const initialRequiredFields = ['eventtype', 'eventName', 'institute1','full_name1', 'email1','phone_number1','level1','department1','semester1','enrollment1'];
-  const [requiredFields, setRequiredFields] = useState([...new Set(initialRequiredFields)]);
-  const [validationErrors, setValidationErrors] = useState({});
-  const [isSubmitting, setIsSubmitting] = useState(false);
+  // const initialRequiredFields = ['eventtype', 'eventName', 'institute1','full_name1', 'email1','phone_number1','level1','department1','semester1','enrollment1'];
+  // const [requiredFields, setRequiredFields] = useState([...new Set(initialRequiredFields)]);
+  // const [validationErrors, setValidationErrors] = useState({});
+  // const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   try {
 
-      const errors = {};
-      requiredFields.forEach((field) => {
-        if (!formData[field]) {
-          errors[field] = `${field} is required`;
-        }
-      });
+  //     const errors = {};
+  //     requiredFields.forEach((field) => {
+  //       if (!formData[field]) {
+  //         errors[field] = `${field} is required`;
+  //       }
+  //     });
 
-      if (Object.keys(errors).length > 0) {
-        // If there are errors, you can handle them here (e.g., show error messages)
-        console.log('Form validation errors:', errors);
-        setValidationErrors(errors);
-        return;
-      }else{
-        setValidationErrors({})
-      }
+  //     if (Object.keys(errors).length > 0) {
+  //       // If there are errors, you can handle them here (e.g., show error messages)
+  //       console.log('Form validation errors:', errors);
+  //       setValidationErrors(errors);
+  //       return;
+  //     }else{
+  //       setValidationErrors({})
+  //     }
   
-      setIsSubmitting(true); // Set isSubmitting to true when form is being submitted
+  //     setIsSubmitting(true); // Set isSubmitting to true when form is being submitted
 
-      const response = await axios.post('api/post', formData);
-      if (response.status === 200) {
-        alert(`Thank you for submitting the form!`);
-        setRegistrationSuccess(true);
-        setSuccessMessage(response.data.text)
-        handleReset();
-      }
-    } catch (error) {
-      console.error(error);
-    }
-    finally {
-      setIsSubmitting(false); // Set isSubmitting back to false after form submission is complete
-    }
-  };
-  const handleReset = () => {
-    setFormData({
-      full_name1: "",
-      email1: "",
-      phone_number1: "",
-      level1: "",
-      department1: "",
-      semester1: "",
-      enrollment1: "",
-      institute1: "",
-      eventtype: "",
-      eventName: "",
-    });
-  };
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    if (name == "eventtype" && value == "solo") {
-      setTypeOfEvent("solo")
-    }
-    if (name == "eventtype" && value == "group") {
-      setTypeOfEvent("group")
-    }
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
+  //     const response = await axios.post('api/post', formData);
+  //     if (response.status === 200) {
+  //       alert(`Thank you for submitting the form!`);
+  //       setRegistrationSuccess(true);
+  //       setSuccessMessage(response.data.text)
+  //       handleReset();
+  //     }
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  //   finally {
+  //     setIsSubmitting(false); // Set isSubmitting back to false after form submission is complete
+  //   }
+  // };
+  // const handleReset = () => {
+  //   setFormData({
+  //     full_name1: "",
+  //     email1: "",
+  //     phone_number1: "",
+  //     level1: "",
+  //     department1: "",
+  //     semester1: "",
+  //     enrollment1: "",
+  //     institute1: "",
+  //     eventtype: "",
+  //     eventName: "",
+  //   });
+  // };
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   if (name == "eventtype" && value == "solo") {
+  //     setTypeOfEvent("solo")
+  //   }
+  //   if (name == "eventtype" && value == "group") {
+  //     setTypeOfEvent("group")
+  //   }
+  //   setFormData((prevData) => ({
+  //     ...prevData,
+  //     [name]: value,
+  //   }));
 
-    setRequiredFields([...new Set([...requiredFields, name])]);
-  };
-  const renderInputRows = () => {
-    const { eventName } = formData;
-    if (!eventName) {
-      return null;
-    }
-    const participantCount = parseInt(eventName.split(" ").pop());
-    //console.log(participantCount)
-    const inputRows = [];
-    // for (let i = 1; i <= participantCount; i++) {
-    //   inputRows.push(
-    //     <InputRow
-    //       key={i}
-    //       formData={formData}
-    //       participantIndex={i}
-    //       handleChange={handleChange}
-    //     />
-    //   );
-    // }
-    let i = 1
-    do {
-      inputRows.push(
-        <InputRow
-          key={i}
-          formData={formData}
-          participantIndex={i}
-          handleChange={handleChange}
-          validationErrors={validationErrors}
-        />
-      );
-      i++
-    } while (i <= participantCount)
-    return inputRows;
-  };
+  //   setRequiredFields([...new Set([...requiredFields, name])]);
+  // };
+  // const renderInputRows = () => {
+  //   const { eventName } = formData;
+  //   if (!eventName) {
+  //     return null;
+  //   }
+  //   const participantCount = parseInt(eventName.split(" ").pop());
+  //   //console.log(participantCount)
+  //   const inputRows = [];
+  //   // for (let i = 1; i <= participantCount; i++) {
+  //   //   inputRows.push(
+  //   //     <InputRow
+  //   //       key={i}
+  //   //       formData={formData}
+  //   //       participantIndex={i}
+  //   //       handleChange={handleChange}
+  //   //     />
+  //   //   );
+  //   // }
+  //   let i = 1
+  //   do {
+  //     inputRows.push(
+  //       <InputRow
+  //         key={i}
+  //         formData={formData}
+  //         participantIndex={i}
+  //         handleChange={handleChange}
+  //         validationErrors={validationErrors}
+  //       />
+  //     );
+  //     i++
+  //   } while (i <= participantCount)
+  //   return inputRows;
+  // };
 
-  const [isChecked, setIsChecked] = useState(false);
-  const [registrationSuccess, setRegistrationSuccess] = useState(false);
-  const [successmessage, setSuccessMessage] = useState("")
+  // const [isChecked, setIsChecked] = useState(false);
+  // const [registrationSuccess, setRegistrationSuccess] = useState(false);
+  // const [successmessage, setSuccessMessage] = useState("")
 
-  const handleRadioButtonChange = (event) => {
-    console.log(event.target.checked)
-    setIsChecked(event.target.checked);
-  };
+  // const handleRadioButtonChange = (event) => {
+  //   console.log(event.target.checked)
+  //   setIsChecked(event.target.checked);
+  // };
 
   return (
     <>
       <div>
         <div className="min-h-screen p-6 bg-gray-100 flex items-center justify-center">
           <div className="w-full  mx-auto min-h-[1000px]">
-            <div>
+            <h1 className="text-red-500">Registration Closed</h1>
+            {/* <div>
               <div className="bg-white text-black  shadow-lg p-4 px-4 md:p-8 mb-2">
                 <div className="text-4xl font-bold">
                   Atmiya Avsar Event Registration
@@ -275,7 +276,7 @@ const page = () => {
                 <div dangerouslySetInnerHTML={{ __html: successmessage }} className="bg-white shadow-lg p-3 px-3 md:p-6 text-red-600 font-bold mb-2">
                 </div>
               )}
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
